84c4cbac38e8db37646c7c3b7fe32053ce881f53
Saumil B Hariyani <saumilhariyani35@gmail.com>
2019-10-14 23:05:23 +0530

    Feature data (#6)
    * date time data made more verbose
    
    * response bug fixed

--- parking/api/namespace/park_api.py
+++ parking/api/namespace/park_api.py
@@ -2,6 +2,7 @@ from flask import request, Response
 from flask_restplus import Resource, Namespace
 import logging
 from injector import inject
+import json_tricks
 
 from parking.api.error_handler import error_handler
 from parking.core.models.parking_entry import ParkingEntryModel
@@ -20,27 +21,33 @@ class ParkingSpace(Resource):
 
     @error_handler
     def get(self):
-        data = request.form.to_dict()
-        arrival = data['arrival']
-        departure = data['departure']
+        data = request.args.to_dict()
+        # print(data)
+        # return Response(status=200)
+        arrival_date = data['arrivalDate']
+        arrival_time = data['arrivalTime']
+        departure_date = data['departureDate']
+        departure_time = data['departureTime']
         vehicle_type = data['vehicleType']
         locality = data['locality']
         city = data['city']
-        country = data['country']
-
-        parking_lot = self.__parking_service.get_parking_lot(locality, city, country)
-        parking_entry = ParkingEntryModel(arrival, departure, vehicle_type)
+        parking_lot = self.__parking_service.get_parking_lot(locality, city)
+        parking_entry = ParkingEntryModel(arrival_date, arrival_time, departure_date, departure_time, vehicle_type)
         if self.__parking_service.availability(parking_entry, parking_lot):
-            r = Response(response=True)
+            r = Response(response=json_tricks.dumps(True), status=200)
+            r.mimetype = 'application/json'
         else:
-            r = Response(response=False)
+            r = Response(response=json_tricks.dumps(False), status=200)
+            r.mimetype = 'application/json'
         return r
 
     @error_handler
     def post(self):
         data = request.form.to_dict()
-        arrival = data['arrival']
-        departure = data['departure']
+        arrival_date = data['arrivalDate']
+        arrival_time = data['arrivalTime']
+        departure_date = data['departureDate']
+        departure_time = data['departureTime']
         vehicle_type = data['vehicleType']
         vehicle_number = data['vehicleNumber']
         locality = data['locality']
@@ -48,9 +55,11 @@ class ParkingSpace(Resource):
         country = data['country']
 
         parking_lot = self.__parking_service.get_parking_lot(locality, city, country)
-        parking_entry = ParkingEntryModel(arrival, departure, vehicle_type, vehicle_number)
+        parking_entry = ParkingEntryModel(arrival_date, arrival_time, departure_date, departure_time, vehicle_type, vehicle_number)
         if self.__parking_service.availability(parking_entry, parking_lot):
-            r = Response(response=True)
+            r = Response(response=json_tricks.dumps(True), status=200)
+            r.mimetype = 'application/json'
         else:
-            r = Response(response=False)
+            r = Response(response=json_tricks.dumps(False), status=200)
+            r.mimetype = 'application/json'
         return r
